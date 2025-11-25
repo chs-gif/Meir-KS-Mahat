f331698ec342d39e36ead98d5b990766b55b5167
shani drabkin <sh0504104723@gmaol.com>
2025-11-25 23:13:44 +0200

    shani drabkin

---
+++ שני דרבקין/hw3.js
@@ -0,0 +1,25 @@
+import fs from 'fs'
+import { promises } from 'fs'
+// קריאה לקובץ data.txt עם קידוד תקין
+promises.readFile('data.txt', 'utf8')
+.then(() =>
+    promises.readFile('file1.txt', 'utf8')
+        .then(data => console.log('data: ', data))
+        .catch(error => console.log('error: ', error))
+)
+// כתיבה סינכרונית
+fs.writeFileSync('file1.txt', 'hello')
+// כתיבה אסינכרונית
+promises.writeFile('file1.txt', 'hello')
+.then(() =>
+    promises.readFile('file1.txt', 'utf8')
+        .then(data => console.log('data: ', data))
+        .catch(error => console.log('error: ', error))
+)
+.catch(error => console.log('error: ', error))
+// אם את רוצה אובייקט JSON – צריך לשים אותו במשתנה
+const myData = {
+    "name": "rachelilililili 😘",
+    "age": "20"
+}
+
